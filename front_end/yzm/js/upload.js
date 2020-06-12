b3a0ee618e24668e21685342449b45055657823f
htxcr0001 <2462442483@qq.com>
2020-06-12 21:28:46 +0800

    ZH: [fix] 根据ZYN的文档修改了接口，但不保证正确性，且图片路径问题为解决

--- front_end/yzm/js/upload.js
+++ front_end/yzm/js/upload.js
@@ -1,52 +1,50 @@
 function uploadFile() {
-	var xmlHttp;
-	if (window.XMLHttpRequest) {
-		xmlHttp = new XMLHttpRequest();
-	} else {
-		xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
-	}
-	xmlHttp.open('post', '/aciton/upload.py', true);//upload.py用于上传验证码 并返回识别结果
-	xmlHttp.send();
-	xmlHttp.onreadystatechange = function() {
-		if (xmlHttp.readyState === 4 && xmlHttp.state === 200) {
-			alert(xmlHttp.responseTest); //xmlHttp.responseTest是验证码结果
-		}
-	}
+	var formData = new FormData();
+	var file = document.getElementById('file').files[0];
+	formData.append("img", file);
+	$.ajax({
+	    url: "/imgProcess/upload",
+	    type: "post",
+	    data: formData,
+	    dataType: "json",
+	    cache: false,
+	    processData: false,
+	    contentType: false, 
+	    success: function (data) {
+			if(data.code==200)
+				alert(data.imgCode);
+			else
+				alet("分析失败！");
+	    }
+	});
 }
 function changetip() {
 	var fileInput = document.getElementById('file');
 	var tip = document.getElementById('tip');
 	tip.innerHTML = fileInput.value;
 }
+var timgCode;
 function gores(){
-	var xmlHttp;
-	if (window.XMLHttpRequest) {
-		xmlHttp = new XMLHttpRequest();
-	} else {
-		xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
-	}
-	xmlHttp.open('post', '/aciton/makeyzm.py', true);//makeyzm.py用于自动生成验证码 并返回 验证码所存路径
-	xmlHttp.send();
-	xmlHttp.onreadystatechange = function() {
-		if (xmlHttp.readyState === 4 && xmlHttp.state === 200) {
-			document.getElementById("myyzm").src = xmlHttp.responseText;
-		}
-	}
+	var formData = new FormData();
+	$.ajax({
+	    url: "/imgProcess/createImg",
+	    type: "post",
+	    data: formData,
+	    dataType: "json",
+	    cache: false,
+	    processData: false,
+	    contentType: false, 
+	    success: function (data) {
+			if(data.code==200)
+				$("#myyzm").attr("src",data.img);
+				timgCode=data.imgCode;
+			else
+				alet("生成失败！");
+	    }
+	});
 }
 function work() {
-	var xmlHttp;
-	if (window.XMLHttpRequest) {
-		xmlHttp = new XMLHttpRequest();
-	} else {
-		xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
-	}
-	xmlHttp.open('post', '/aciton/vyzm.py', true);//vyzm.py用于识别验证码并返回识别结果
-	xmlHttp.send();
-	xmlHttp.onreadystatechange = function() {
-		if (xmlHttp.readyState === 4 && xmlHttp.state === 200) {
-			alert(xmlHttp.responseTest); //xmlHttp.responseTest是验证码结果
-		}
-	}
+	alert(timgCode);
 }
 
 
