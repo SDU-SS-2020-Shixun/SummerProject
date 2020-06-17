74f9362bbafd025664bf249c200f751f341180e8
htxcr0001 <2462442483@qq.com>
2020-06-17 16:21:05 +0800

    ZH: [fix] 修改了YN的setting.py文件

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1&5e0l6egtjspp_%sujr!%pvz)lceal=0mpt61lvwzu(vgin4v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
